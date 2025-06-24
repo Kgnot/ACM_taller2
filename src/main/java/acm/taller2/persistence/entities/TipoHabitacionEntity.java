aad0c7ad01f96d39d687f27db90061fefd0a05a1
Nicolás Yepes <nayepesc@udistrital.edu.co>
2025-06-23 22:39:34 -0500

    feat: Se agregaron al package entities, todas las clases que representan a las entidades del modelo de la BD, con sus constructores, getters, setters y anotaciones de JPA

---
+++ src/main/java/acm/taller2/persistence/entities/TipoHabitacionEntity.java
@@ -0,0 +1,61 @@
+package acm.taller2.persistence.entities;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+@Table(name="tipo_habitacion")
+public class TipoHabitacionEntity implements Serializable {
+    @Id
+    @Column(name="id_tipo_habitacion")
+    private Long id;
+    private String nombre;
+    private Integer cantidad;
+
+    @ManyToOne
+    @JoinColumn(name="fk_id_hotel")
+    @JsonBackReference("hotel-tipoHabitaciones")
+    private HotelEntity hotel;
+
+    @OneToMany(mappedBy = "tipoHabitacion")
+    @JsonManagedReference("tipoHabitacion-habitaciones")
+    private List<HabitacionEntity> habitaciones = new ArrayList<>();
+
+    public TipoHabitacionEntity() {
+    }
+
+    public TipoHabitacionEntity(Long id, String nombre, Integer cantidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+}
